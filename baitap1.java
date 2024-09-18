2b0f477cb844a22a877f6910933094abb27e3694
Viiuuuu <nguyenhavyy0105@gmail.com>
2024-09-18 15:36:40 +0700

    Add files via upload

---
+++ baitap1.java
@@ -0,0 +1,12 @@
+package baitap1;
+
+import java.util.Scanner;
+
+public class baitap1 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("nhập tên của bạn: ");
+		String name=sc.nextLine();
+		System.out.println("Hell "+name);
+	}
+}
