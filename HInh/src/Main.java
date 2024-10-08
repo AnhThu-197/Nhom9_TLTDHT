8a42af22f156768e9d2e4cfe8b399b161016046d
thuong1511 <nguyenthithuong15112005@gmail.com>
2024-10-08 23:36:44 +0700

    Delete HInh directory

--- HInh/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        bt1 hcnA = new bt1();
-        System.out.println("DIen tich hinh chu nhat la: " + hcnA.dientich());
-        System.out.println("Chu vi hinh chu nhat la :" + hcnA.chuvi());
-    }
-}
